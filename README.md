9ea4ef043a225e788fcb8c299ab539b8ad97f89a
obble <mkentnotpaul@gmail.com>
2017-11-20 10:23:43 +0000

    1
    init

---
+++ README.md
@@ -0,0 +1,5 @@
+modguide
+======
+![Demo](http://i.pi.gy/b0ppx.png)
+======
+sticks [VanillaGuide](https://github.com/rsheep/VanillaGuide "VanillaGuide 1.12.1") into a chat window
---
+++ guide.lua
@@ -0,0 +1,55 @@
+	
+	_G 		= getfenv(0)
+	local H = {}
+
+	H.FCF_SelectDockFrame = FCF_SelectDockFrame
+
+	local SelectDockFrame = function(f)
+		H.FCF_SelectDockFrame(f)
+		local vg    = _G['VG_MainFrame']
+        local vgs 	= _G['VG_SettingsFrame']
+        if  vg then
+			if  f == ChatFrame3 then
+	            vg:Show()
+	        	if  vgs.showthis then
+	        		vgs:Show()
+	        	end
+	        else
+	        	vg:Hide()
+	            if  vgs:IsVisible() then
+	        		vgs.showthis = true
+	        		vgs:Hide()
+	        	end
+			end
+		end
+	end
+
+	local AddGuide = function(parent)
+		local vg = _G['VG_MainFrame']
+		vg:Hide()
+		vg:RegisterForDrag(nil)
+		vg:SetPoint('TOPLEFT', 		parent)
+		vg:SetPoint('BOTTOMRIGHT',	parent, 0, -6)
+		vg:SetBackdrop(nil)
+		vg:SetScript('OnMouseDown', nil)
+
+        for _,  v in pairs({vg:GetChildren()}) do
+            if  v:GetBackdrop() then
+                v:SetBackdrop(nil)
+            end
+        end
+	end
+
+	local OnEvent = function(n)
+		SetChatWindowShown(3, true)
+		SetChatWindowName(3,  'Quests')
+		ChatFrame_RemoveAllMessageGroups(ChatFrame3)
+		ChatFrame_RemoveAllChannels(ChatFrame3)
+		AddGuide(ChatFrame3)
+	end
+
+	FCF_SelectDockFrame = SelectDockFrame
+
+	local e = CreateFrame'Frame'
+	e:RegisterEvent'PLAYER_LOGIN'
+	e:SetScript('OnEvent', OnEvent)
